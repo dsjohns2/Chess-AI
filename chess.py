68229ee8e0955998ed7cdb3692eadcc930c6ff36
Dan Johnson <dsjohns2@illinois.edu>
2018-01-04 15:38:15 -0600

    Add machine learning base code

--- chess.py
+++ chess.py
@@ -12,7 +12,7 @@ class square:
 		self.piece = piece
 
 class game:
-	def __init__(self):
+	def __init__(self, weights):
 		self.board = [[square("b", "R", True), square("b", "N", True), square("b", "B", True), square("b", "Q", True), square("b", "K", True), square("b", "B", True), square("b", "N", True), square("b", "R", True)], 
 					  [square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True)], 
 					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
@@ -21,21 +21,63 @@ class game:
 					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
 					  [square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True)], 
 					  [square("w", "R", True), square("w", "N", True), square("w", "B", True), square("w", "Q", True), square("w", "K", True), square("w", "B", True), square("w", "N", True), square("w", "R", True)]] 
+		"""
+		self.board = [[square("b", "R", True), square("b", "N", True), square("b", "B", True), square("b", "Q", True), square("b", "K", True), square("b", "B", True), square("b", "N", True), square("b", "R", True)], 
+					  [square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True), square("b", "P", True)], 
+					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+					  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+					  [square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True), square("w", "P", True)], 
+					  [square("w", "R", True), square("w", "N", True), square("w", "B", True), square("w", "Q", True), square("w", "K", True), square("w", "B", True), square("w", "N", True), square("w", "R", True)]] 
+		"""
 		self.white_turn = True
 		self.turns_since_capture_or_pawn_movement = 0
 		self.pieces = 32
+		self.weights = weights
+		self.history = []
+		self.training_set = []
 
 	def make_move(self, human_flag):
+		"""
+		returns -1 if game is still in progess
+				0 if game is a tie
+				1 if white wins
+				2 if black wins
+		"""
+		self.history.append(self.board)
 		if(self.white_turn):
 			legal_moves = self.get_legal_moves(self.board, "w")
-			legal_moves = self.remove_moves_resulting_in_check(legal_moves, "w")
 		else:
 			legal_moves = self.get_legal_moves(self.board, "b")
-			legal_moves = self.remove_moves_resulting_in_check(legal_moves, "b")
 
-		#End of Game
-		if(len(legal_moves) == 0):
+		#Check for End of Game
+		game_is_over = self.game_is_over(self.board, legal_moves, self.white_turn)
+		if(not (game_is_over == -1)):
+			return game_is_over
+		#Game Continues
+		else:
 			if(self.white_turn):
+				random_flag = False
+			else:
+				random_flag = True
+			move = self.pick_move(legal_moves, human_flag, random_flag)
+			self.upgrade_pawns(move)
+			self.board = move
+			self.board = self.check_for_stalemate(self.board)
+			self.white_turn = not self.white_turn
+			return -1
+
+	def game_is_over(self, board, legal_moves, white_turn):
+		"""
+		returns -1 if game is still in progess
+				0 if game is a tie
+				1 if white wins
+				2 if black wins
+		"""
+		#Check for End of Game
+		if(len(legal_moves) == 0):
+			if(white_turn):
 				king_color = "w"
 				opposite_color = "b"
 			else:
@@ -44,34 +86,35 @@ class game:
 
 			for i in range(0, 8):
 				for j in range(0, 8):
-					if(self.board[i][j].piece == "K" and self.board[i][j].color == king_color):
+					if(board[i][j].piece == "K" and board[i][j].color == king_color):
 						king_i = i
 						king_j = j
 			danger_squares = []
 			for i in range(0, 8):
 				for j in range(0, 8):
-					danger_squares += self.squares_that_piece_at_i_j_can_take(self.board, i, j, opposite_color)
+					danger_squares += self.squares_that_piece_at_i_j_can_take(board, i, j, opposite_color)
 			checkmate_flag = False
 			for danger_square in danger_squares:
 				if(danger_square[0] == king_i and danger_square[1] == king_j):
 					checkmate_flag = True
-			if(checkmate_flag):
-				if(self.white_turn):
-					print("Black Wins!")
-				else:
-					print("White Wins!")
-			else:
-				print("Stalemate - No possible moves")
 
-			print("Game Over.")
-			sys.exit(0)
-		#Game Continues
+			if(checkmate_flag):
+				if(white_turn):
+					return 2
+				else:
+					return 1
+			else:
+				return 0
 		else:
-			move = self.pick_move(legal_moves, human_flag)
-			self.upgrade_pawns(move)
-			self.board = move
-			self.check_for_stalemate(self.board)
-			self.white_turn = not self.white_turn
+			num_pieces = 0
+			for i in range(0, 8):
+				for j in range(0, 8):
+					if(board[i][j].is_occupied):
+						num_pieces += 1
+			if(num_pieces == 2):
+				return 0
+			else:
+				return -1
 
 	def check_for_stalemate(self, board):
 		#stalemate for 50 move rule
@@ -88,11 +131,18 @@ class game:
 			self.pieces = pieces
 			self.turns_since_capture_or_pawn_movement = 0
 
-		if(self.turns_since_capture_or_pawn_movement >= 50):
-			self.print_board(board)
-			print("Stalemate - No capture in 50 moves")
-			print("Game Over.")
-			sys.exit(0)
+		if(self.turns_since_capture_or_pawn_movement >= 100): #TODO: change back to 50
+			stalemate_board = [[square("b", "K", True), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("w", "K", True), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)], 
+							  [square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False), square("", "", False)]] 
+			return stalemate_board
+		else:
+			return board
 	
 	def upgrade_pawns(self, board):
 		for i in range(0, 8):
@@ -381,9 +431,10 @@ class game:
 						self.remove_piece(i, j, new_board)
 						self.add_piece(elem[0], elem[1], board[i][j].piece, board[i][j].color, new_board)
 						legal_moves.append(new_board)
+		legal_moves = self.remove_moves_resulting_in_check(legal_moves, color)
 		return legal_moves
 
-	def pick_move(self, legal_moves, human_flag):
+	def pick_move(self, legal_moves, human_flag, random_flag):
 		if(human_flag):
 			human_move = ""
 			while(not len(human_move) == 3):
@@ -412,8 +463,54 @@ class game:
 				move = legal_moves[k]
 				if(move[i][j].piece == piece):
 					return legal_moves[k]
-		else:
+		elif(random_flag):
 			return legal_moves[random.randrange(0, len(legal_moves))]
+		else:
+			#use learned function
+			move_values = []
+			for move in legal_moves:
+				cur_value = self.lin_fun(move)
+				move_values.append(cur_value)
+
+			best_val = move_values[0]
+			best_idx = 0
+			for i in range(0, len(move_values)):
+				if(move_values[i]>best_val):
+					best_val = move_values[i]
+					best_idx = i
+			return legal_moves[best_idx]
+
+	def lin_fun(self, board):
+		x = self.get_x(board)
+		ret_val = 0
+		for i in range(0, len(self.weights)):
+			ret_val += self.weights[i] * x[i]
+		return ret_val
+
+	def get_x(self, board):
+		#x[0] = num white points on board
+		#x[1] = num black points on board
+		x = [0, 0]
+		for i in range(0, 8):
+			for j in range(0, 8):
+				if(board[i][j].piece == "P"):
+					points = 1
+				elif(board[i][j].piece == "N"):
+					points = 3
+				elif(board[i][j].piece == "B"):
+					points = 3
+				elif(board[i][j].piece == "R"):
+					points = 5
+				elif(board[i][j].piece == "Q"):
+					points = 9
+				else:
+					points = 0
+
+				if(board[i][j].color == "w"):
+					x[0] += points
+				else:
+					x[1] += points
+		return x
 
 	def print_board(self, board):
 		print(Fore.WHITE + "~~~~~~~~~~~~~~~~~")
@@ -433,20 +530,77 @@ class game:
 		print("")
 
 	def human_move(self):
-		self.make_move(True)
+		return self.make_move(True)
 
 	def computer_move(self):
-		self.make_move(False)
+		return self.make_move(False)
 
-my_game = game()
-my_game.print_board(my_game.board)
-for i in range(0, 1000):
-	#white
-	my_game.computer_move()
-	my_game.print_board(my_game.board)
-	#black
-	my_game.computer_move()
-	my_game.print_board(my_game.board)
+	def play_game(self):
+		game_is_over = -1
+		while(game_is_over == -1):
+			game_is_over = my_game.computer_move()
+			#time.sleep(.1)
+			#print(game_is_over)
+			#my_game.print_board(my_game.board)
+		return game_is_over
+
+	def create_training_set(self):
+		for i in range(0, len(self.history)):
+			if(i % 2 == 0):
+				state = self.history[i]
+				legal_moves = self.get_legal_moves(state, "w")
+				if(self.game_is_over(state, legal_moves, True) == 1):
+					self.training_set.append([state, 100])
+				if(self.game_is_over(state, legal_moves, True) == 2):
+					self.training_set.append([state, -100])
+				if(self.game_is_over(state, legal_moves, True) == 0):
+					self.training_set.append([state, 0])
+				if(self.game_is_over(state, legal_moves, True) == -1):
+					if(i+2<len(self.history)):
+						self.training_set.append([state, self.lin_fun(self.history[i+2])])
+					else:
+						next_state = self.history[i+1]
+						next_state_legal_moves = self.get_legal_moves(state, "b")
+						if(self.game_is_over(next_state, next_state_legal_moves, False) == 2):
+							self.training_set.append([state, -100])
+						else:
+							self.training_set.append([state, 0])
+
+	def shift_weights(self):
+		change_constant = 0.0001
+		for elem in self.training_set:
+			x = self.get_x(elem[0])
+			#print(x)
+			for j in range(0, len(self.weights)):
+				add_to_weight = change_constant * (elem[1] - self.lin_fun(elem[0])) * x[j]
+				self.weights[j] += add_to_weight
+
+weights = [.5, .5]
+for i in range(0, 100):
+	print(weights)
+	my_game = game(weights)
+	my_game.play_game()
+	my_game.create_training_set()
+	my_game.shift_weights()
+	weights = my_game.weights
+print(weights)
+
+white_wins = 0
+black_wins = 0
+draws = 0
+for i in range(0, 100):
+	my_game = game(weights)
+	result = my_game.play_game()
+	if(result == 0):
+		draws += 1
+	elif(result == 1):
+		white_wins += 1
+	elif(result == 2):
+		black_wins += 1
+print("white wins: ", white_wins)
+print("black wins: ", black_wins)
+print("draws: ", draws)
+
 #TODO:
 	##fine tune human moves
 	##not recognizing checkmate?
@@ -455,4 +609,6 @@ for i in range(0, 1000):
 	##ml
 		###how many white pieces cover other white pieces
 		###points
+		###num pieces
+		###num available moves
 		###num pieces
